723f80d47c3c917ea71a7f9585844a445de52db3
Mirsaid04 <mirsaidd04@gmail.com>
2023-11-15 17:21:21 +0900

    Add project files.

---
+++ PreparingBreakfastAsync/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PreparingBreakfastAsync
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
